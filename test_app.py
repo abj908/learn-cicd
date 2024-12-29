2fbe0752315d5cbb54e5fd4e4c2faf4d60bb2f51
Albert Joseph <70643134+abj908@users.noreply.github.com>
2024-12-29 17:17:20 +0000

    update reqs

--- test_app.py
+++ test_app.py
@@ -1,10 +1,14 @@
 import unittest
-from app import add_numbers
+from app import add_numbers, subtract_numbers
 
 class TestApp(unittest.TestCase):
     def test_add_numbers(self):
         self.assertEqual(add_numbers(2, 3), 5)
         self.assertEqual(add_numbers(-1, 1), 0)
+
+    def test_subtract_numbers(self):  # New test
+        self.assertEqual(subtract_numbers(5, 3), 2)
+        self.assertEqual(subtract_numbers(0, 3), -3)
 
 if __name__ == "__main__":
     unittest.main()
